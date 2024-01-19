f4ca0cc22b8484f42d1d9aa263fb5071be928df5
wSerendipty <2251638767@qq.com>
2024-01-19 19:08:57 +0800

    TEST:测试不添加

---
+++ src/views/menu/problems/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    这是管理员页面
+    <CodeEditorVue :height="'300px'" :width="'800px'" :on-change="handleChange" :value="value"></CodeEditorVue>
+  </div>
+
+</template>
+
+<script setup>
+
+import CodeEditorVue from "../../components/CodeEditorVue.vue";
+import {ref} from "vue";
+
+const value = ref('')
+
+const handleChange = (v) => {
+  console.log(v)
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ src/views/problem/do/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    这是题库页面
+  </div>
+
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
